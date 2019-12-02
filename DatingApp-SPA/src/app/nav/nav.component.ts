2c2d13067406bcbfb3706ed9d2490c016bada9ca
LloydNicholson <34131158+LloydNicholson@users.noreply.github.com>
2019-12-02 08:57:07 +0200

    End of Section 4

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -19,7 +19,7 @@ export class NavComponent implements OnInit {
     this.auth.login(this.model).subscribe(next => {
       console.log('Logged in successfully');
     }, err => {
-      console.log('Failed to login');
+      console.log(err);
     });
   }
 
